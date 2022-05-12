63355447fea84e36323d3294edbfad252eedb2ae
Bastien Nocera <hadess@hadess.net>
2022-05-12 15:51:37 +0200

    Build local Flatpak on the test branch
    Stops it from clobbering the stable release if installed.

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+BRANCH=test
+
 rm -f org.ghidra_sre.Ghidra.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean _build org.ghidra_sre.Ghidra.json --repo=_repo
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build org.ghidra_sre.Ghidra.json --repo=_repo
