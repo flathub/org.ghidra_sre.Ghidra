cbc47ad524fe37de2b97e3625886ef54849f81da
Matthieu CHARETTE <matthieu.charette@gmail.com>
2022-11-06 15:41:44 +0100

    Do not specify SDK branch in generate-deps.sh

--- generate-deps.sh
+++ generate-deps.sh
@@ -3,7 +3,7 @@
 rm -rf _deps_build
 git clone https://github.com/NationalSecurityAgency/ghidra.git -b stable _deps_build
 cd _deps_build
-echo "source /usr/lib/sdk/openjdk11/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel org.freedesktop.Sdk//21.08
+echo "source /usr/lib/sdk/openjdk11/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel org.freedesktop.Sdk
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/wip/hadess/add-gradle/gradle/flatpak-gradle-generator.py
 chmod +x flatpak-gradle-generator.py
 ./flatpak-gradle-generator.py gradle-log.txt ../gradle-dependencies.json --destdir dependencies/flatRepo --arches x86_64,aarch64
