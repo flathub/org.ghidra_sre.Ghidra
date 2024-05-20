d64f0bfee9f7c369ee5195df3e60a3c72fa3f91d
Bastien Nocera <hadess@hadess.net>
2024-05-20 19:44:41 +0200

    Check for SDK and JDK being installed
    This makes the generate-deps script a bit more robust by checking
    whether org.freedesktop.Sdk and the associated JDK are installed.

--- generate-deps.sh
+++ generate-deps.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
+get_sdk()
+{
+	OUTPUT=$(flatpak info org.freedesktop.Sdk 2>&1)
+	RET=$?
+
+	if [ $RET -eq 0 ] ; then
+		echo org.freedesktop.Sdk
+	else
+		echo $OUTPUT | awk '{print $NF}'
+	fi
+}
+
+verify_jdk()
+{
+	JDK=$(flatpak info $SDK | grep "Ref:" | sed -e 's,Ref: runtime/,,' -e 's,org.freedesktop.Sdk,org.freedesktop.Sdk.Extension.openjdk17,')
+	OUTPUT=$(flatpak info $JDK 2>&1)
+	if [ $? -ne 0 ] ; then
+		echo "$JDK extension missing for runtime $SDK"
+		exit 1
+	fi
+}
+
+SDK=$(get_sdk)
+verify_jdk
+
 rm -rf _deps_build
 git clone https://github.com/NationalSecurityAgency/ghidra.git -b stable _deps_build
 cd _deps_build
-echo "source /usr/lib/sdk/openjdk17/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel org.freedesktop.Sdk
+echo "source /usr/lib/sdk/openjdk17/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel $SDK
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/wip/hadess/add-gradle/gradle/flatpak-gradle-generator.py
 chmod +x flatpak-gradle-generator.py
 ./flatpak-gradle-generator.py gradle-log.txt ../gradle-dependencies.json --destdir dependencies/flatRepo --arches x86_64,aarch64
