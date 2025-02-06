2feabb116f5f9d97996de2f96df180b83e210391
Bastien Nocera <hadess@hadess.net>
2025-02-06 19:10:11 +0100

    Update generate-deps script for newer Gradle
    Fixes "Aborting build initialization due to existing files in the
    project directory" warning.
    
    See https://docs.gradle.org/8.9/release-notes.html#build-init-does-not-override-existing-build-files-by-default

--- generate-deps.sh
+++ generate-deps.sh
@@ -28,7 +28,7 @@ verify_jdk
 rm -rf _deps_build
 git clone https://github.com/NationalSecurityAgency/ghidra.git -b stable _deps_build
 cd _deps_build
-echo "source /usr/lib/sdk/openjdk21/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel $SDK
+echo "source /usr/lib/sdk/openjdk21/enable.sh && gradle -g gradle-cache -I gradle/support/fetchDependencies.gradle init --overwrite && rm -rf gradle-cache && gradle -g gradle-cache --info --console plain buildGhidra > gradle-log.txt" | flatpak run --share=network --filesystem=`pwd` --devel $SDK
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/gradle/flatpak-gradle-generator.py
 chmod +x flatpak-gradle-generator.py
 ./flatpak-gradle-generator.py gradle-log.txt ../gradle-dependencies.json --destdir dependencies/flatRepo --arches x86_64,aarch64
